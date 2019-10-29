cd0c9ebd9f85b21fa290922cd1414511004a4c0c
Timothy Novak <tnovak@olin.edu>
2019-10-28 22:48:51 -0400

    Update the doumentation for functions and classes

--- main.py
+++ main.py
@@ -11,13 +11,21 @@ SCREEN_HEIGHT = 600
 
 #MARK: view
 class View:
-    """Handle updates to the user display"""
-    
+    """Handle updates to the user display
+
+    Properties:
+    (int) width: the width of the pygame screen
+    (int) height: the height of the pygame screen
+    (tuple) backgroundColor: the color of the background of the screen in rgb
+    ([GameObject]) objects: a list of objects in the scene
+    (pygame.display) window: the pygame Window
+    (pygame.surface) background: the background of the Window
+    """
+
 
     def __init__(self,screenWidth = SCREEN_WIDTH, screenHeight = SCREEN_HEIGHT, backgroundColor = (0,0,0)):
         """Set up the View
 
-        Keyword arguments:
         screenwidth(optional) -- the width of the screen generated in pixels
         screenHeight(optional) -- the height of the screen generated in pixels
         backgroundColor(optional) -- the color in rgb of the background of the screen"""
@@ -31,7 +39,7 @@ class View:
         self.initWindow()
 
     def initWindow (self):
-        """A function which creates a pygame window specified by the properties of the view."""
+        """Creates a pygame window specified by the properties of the view."""
         #MARK: Window setup
         #create the pygame window of the designated size
         print(self.width)
@@ -45,7 +53,7 @@ class View:
         self.background = self.background.convert()
 
     def draw(self):
-        """a method which draws all of the pygame surfaces"""
+        """Draws all of the pygame surfaces"""
         self.window.blit(self.background, (0,0))
 
         for obj in self.objects:
@@ -55,18 +63,27 @@ class View:
         pygame.display.flip()
 
     def addObj(self, obj):
-        """a method which adds an object to the lit of objects to draw"""
+        """Adds an object to the list of objects to draw
+
+        (GameObject) obj: the object to add"""
         self.objects.append(obj)
 
 #MARK: controller
 class Controller:
+    """Handle user inputs
+
+    Properties:
+    (bool) isRunning: stores whether or not the main game loop is running
+    (tuple) mousePos: stores the position of the mouse in the pygame window"""
 
     def __init__(self):
         self.isRunning = True
         self.mousePos = ()
 
     def handleEvent(self,event):
-        """Handle the events from the pygame event colors"""
+        """Handle the pygame events from the controller
+
+        (pygame.event) event: the event to process"""
         #checks for the x in the corner
         if event.type == pygame.QUIT:
             #stop looping
@@ -86,7 +103,16 @@ class Controller:
             self.isRunning = True
 
     def getControlInput(self,input,vs,tracker):
-        """gets the posion that the controller is at"""
+        """get the position that the controller is at
+
+        (string) input: the input source to get data from
+        (videoStream) vs: the videop stream from the camera
+        (tracker) tracker: the oject tracker which identifies the location of an
+         object in the stream
+
+         (tuple) returns: the x and y positions of the controller input"""
+
+        #check inputs
         if input == 'pygame':
             return self.mousePos
         elif input == 'openCV':
@@ -96,16 +122,24 @@ class Controller:
 
 #MARK: Model
 class Model:
+    """Handle back end calculations
+
+    Properties:
+    (view) view: the view object for the program
+    (Controller) controller: the controller object for the program"""
+
     def __init__(self):
         #create view, and controller objects
         self.view = View(640,700)
         self.controller = Controller()
 
     def getBallPos(self, currentX, currentY, dt):
-        '''
-        This function encapsulates the physics of the ball motion.
+        '''Encapsulates the physics of the ball motion.
+
         (numpy.array) currentX: the current x position of the ball
         (numpy.array) currentY: the current y position of the ball
+        (numpy.array) dt: the array of time steps for each position of the ball
+
         (tuple) returns: new x and y
         '''
         # TODO: determine the acceleration of the ball
@@ -122,13 +156,19 @@ class Model:
         if xAcc >= cutoff and xAcc < threshold:
             return xVel[-1], yVel[-1], True
         else:
-            return currentX[-1], currentY[-1], False      
+            return currentX[-1], currentY[-1], False
 
     def throwBall(self, xVel, yVel, xInit, yInit, tEnd, goal):
         ACCELERATION = 100
+        '''Computes trajectories which are displayed to the user
+
+        (numpy.array) xVel: an array of the x axis velocities in time
+        (numpy.array) yVel: an array of the y axis velocities in time
+        (int) xInit: the inital x posiion of the ball on throw
+        (int) yInit: the inital y posiion of the ball on throw
+        (float) tEnd:the ending time of the movement
+        (Goal) goal: the game object describing the basket ball hoop
         '''
-        This function takes in velocities and computes trajectories that are then displayed to the user
-        '''  
         times = np.linspace(0, tEnd, 20*tEnd)
         xPos = np.ones(len(times)+1)
         xPos[0] = xInit
@@ -137,7 +177,7 @@ class Model:
         for i in range(len(times)):
             xPos[i+1] = xPos[i] + (xVel/40)*times[i]
             yPos[i+1] = yPos[i] + (yVel/40)*times[i] + 0.5*ACCELERATION*times[i]**2
-        
+
         ball = Ball(self.view)
         ball.visible = True
 
@@ -147,6 +187,9 @@ class Model:
             self.view.draw()
 
     def runGameLoop(self):
+        """Sets up and executes the main game loop
+        """
+
         #create the game objects
 
         goal = Goal(self.view)
@@ -168,13 +211,13 @@ class Model:
         newTracker = tracker.newTracker()
         vs = VideoStream(src=0).start()
         newTracker = tracker.newTracker()
-        
+
         #MARK: gameLoop
         currentX = []
         currentY = []
         timesteps = []
         N = 10
-        
+
         while running:
             startTime = time.time()
             #MARK: event listeners
@@ -183,8 +226,8 @@ class Model:
                 #handle events
                 self.controller.handleEvent(event)
                 running = self.controller.isRunning
-            
-            
+
+
             #update positions
             cntPos = self.controller.getControlInput('openCV',vs,newTracker)
             endTime = time.time()
@@ -216,7 +259,7 @@ class Model:
                 currentX.append(cntPos[0])
                 currentY.append(cntPos[1])
                 timesteps.append(dt)
-            
+
             if cntPos != None:
                 ball.pos = cntPos
 
@@ -229,8 +272,27 @@ class Model:
 
 #MARK: object classes
 class GameObject:
+    """Base functionality for displayed objects
+
+    Properties:
+    (view) view: the view that this belongs to
+    (tuple) pos: the x and y cordinates of the top left corner of the object
+    (tuple) color: the set of rgb which define the object's coloer
+    (tuple) geometry: the extra arguments which define the geometry of the shape
+    these are in the order of (string) name which identifies the type of geometry
+    then any suplemental arguments which are necessary for drawing that shape
+    (bool) visible: this discribes whether or not the object should be drawn"""
 
     def __init__(self,view, pos = None,color = (255,255,255), *geometry):
+        """creates a game object
+
+        (view) view: the view that this belongs to
+        (tuple) pos: the x and y cordinates of the top left corner of the object
+        (tuple) color: the set of rgb which define the object's coloer
+        (tuple) geometry: the extra arguments which define the geometry of the shape
+        these are in the order of (string) name which identifies the type of geometry
+        then any suplemental arguments which are necessary for drawing that shape"""
+
         #if no positional argument was supplied
         if pos == None:
             #set the default position
@@ -256,10 +318,11 @@ class GameObject:
             self.surface = pygame.Surface((self.geometry[1],self.geometry[2]),pygame.SRCALPHA, 32)
 
         #add the object to the view
-        view.addObj(self)
+        self.view.addObj(self)
 
-    def draw(self, view):
-        """A method which draws its parent object to the screen if the object is supposed to be drawn"""
+    def draw(self):
+        """Draws its parent object to the screen if the object is supposed to be drawn"""
+
         if self.visible:
             if self.geometry[0] == 'circle':
                 #draw the circle on to the surface
@@ -272,35 +335,35 @@ class GameObject:
             self.surface = self.surface.convert_alpha()
 
             #display the circle
-            view.window.blit(self.surface,self.pos)
+            self.view.window.blit(self.surface,self.pos)
         else:
             return
 
 
 class Ball (GameObject):
-    """A class which draws a ball on the screen"""
+    """Draws a ball on the screen"""
+
     def __init__(self,view, pos = [300,300],color = (255,140,0),radius = 30):
-        self.mass = 1
         super().__init__(view,pos,color,'circle',radius,0)
 
 
 class Goal (GameObject):
-    """A class which draws a goal on the screen"""
+    """Draws a goal on the screen"""
+
     def __init__(self,view, pos = [700,300],color = (255,255,255),width = 70,height = 30,thickness = 2):
 
         super().__init__(view,pos,color,'rectangle',width,height,thickness)
-    
+
     def ballInGoal(self, ball):
         """Check if the a ball is in the goal
 
         (Ball) ball: the ball to check if there is a goal
 
-        Returns:
-        (bool):True if there is a ball in the goal and false if there isn`t"""
+        (bool) returns: True if there is a ball in the goal and false if there isn`t"""
         goalState = False
         #if the ball is horrizontally in the hoop
         if ball.pos[0] >= self.pos[0] and ball.pos[0]+ball.surface.get_width() <= self.pos[0]+self.surface.get_width() and ball.visible:
-            
+
                 #if the top of the ball is below the top surface and above the bottom surface of the goal
             if ball.pos[1] >= self .pos[0] and ball.pos[1] <= self.surface.get_height()+self.pos[1]:
                 ballState = True
@@ -318,14 +381,10 @@ class Goal (GameObject):
         else:
             ballState = False
 
-class Button (GameObject):
-    """A class which draws a button on the screen"""
-    def __init__(self,view, pos = [300,300],color = (255,255,255),width = 90,height = 70,thickness = 0):
-
-        super().__init__(view,pos,color,'rectangle',width,height,thickness)
-
 def runGame(argv):
-    """A function which runs the game when called"""
+    """Runs the game when called
+    
+    (tuple) argv: any command line arguments which were passed to the program"""
     model = Model()
     model.runGameLoop()
 
